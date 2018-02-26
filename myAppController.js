66c4e7f67ac7ac4082e38671d2934e825519121d
AnkitaInnolat <ankita@innolat.com>
2018-02-26 12:34:43 +0530

    Initial Commit
    Initial Commit

---
+++ myAppController.js
@@ -0,0 +1,36 @@
+﻿var app = angular.module("myApp", ['ngCookies']);
+
+app.controller('calculateController', function ($scope, $cookieStore) {
+    $scope.result = "";
+
+    $scope.calculateResult = function (number1, number2, operation) {
+        $cookieStore.put("Number1", number1);
+        $cookieStore.put("Number2", number2);
+      
+        if (operation == 'Add') {
+
+            $scope.result = number1 + number2;
+
+        } else if (operation == 'Substract') {
+
+            $scope.result = number1 - number2;
+
+        } else if (operation == 'Divide') {
+
+            $scope.result = number1 / number2;
+
+        } else {
+
+            $scope.result = number1 * number2;
+
+        }
+
+        $cookieStore.put("result", $scope.result);
+
+    }
+
+
+
+
+
+});
